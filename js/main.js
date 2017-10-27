03433fcc8fb2e9bf7894200f0a80558ac05462df
villedevs <villedevs@gmail.com>
2017-10-27 12:04:18 +0300

    uusi

--- js/main.js
+++ js/main.js
@@ -1,70 +1,40 @@
-document.querySelector("#sub").addEventListener('click', (evt) => {
-  let fail = false;
+"use strict";
 
-  const etunimi = document.querySelector("#etunimi");
-  const sukunimi = document.querySelector("#sukunimi");
-  const email = document.querySelector("#email");
-  const puhelin = document.querySelector("#puhelin");
-  const posti = document.querySelector("#posti");
-  const salasana = document.querySelector("#salasana");
-
-  const email_re = new RegExp('^\\S+@\\S+\\.\\S+$');
-  const puhelin_re = new RegExp('^\\+\\d+$');
-  const posti_re = new RegExp('^\\d{5}$');
-
-  etunimi.style.backgroundColor = "white";
-  etunimi.style.color = "black";
-  sukunimi.style.backgroundColor = "white";
-  sukunimi.style.color = "black";
-  email.style.backgroundColor = "white";
-  email.style.color = "black";
-  puhelin.style.backgroundColor = "white";
-  puhelin.style.color = "black";
-  posti.style.backgroundColor = "white";
-  posti.style.color = "black";
-  salasana.style.backgroundColor = "white";
-  salasana.style.color = "black";
+let lomakeOK = 0;
 
 
-  if (etunimi.value.length == 0)
+const checkAttribute = (attr, elements, func) =>
+{
+  elements.forEach((el) =>
   {
-    etunimi.style.backgroundColor = "red";
-    etunimi.style.color = "white";
-    fail = true;
+    if (el.hasAttribute(attr))
+    {
+      func(el);
+    }
   }
-  else if (sukunimi.value.length == 0)
-  {
-    sukunimi.style.backgroundColor = "red";
-    sukunimi.style.color = "white";
-    fail = true;
-  }
-  else if (email.value.length == 0 || email_re.exec(email.value) == null)
-  {
-    email.style.backgroundColor = "red";
-    email.style.color = "white";
-    fail = true;
-  }
-  else if (puhelin.value.length == 0 || puhelin_re.exec(puhelin.value) == null)
-  {
-    puhelin.style.backgroundColor = "red";
-    puhelin.style.color = "white";
-    fail = true;
-  }
-  else if (posti.value.length == 0 || posti_re.exec(posti.value) == null)
-  {
-    posti.style.backgroundColor = "red";
-    posti.style.color = "white";
-    fail = true;
-  }
-  else if (salasana.value.length == 0)
-  {
-    salasana.style.backgroundColor = "red";
-    salasana.style.color = "white";
-    fail = true;
-  }
+  );
+};
 
-  if (fail)
+const checkEmpty = (el) =>
+{
+  console.log(el);
+  if (el.value.length === 0)
   {
-    evt.preventDefault();
+    el.setAttribute('style', 'border: 2px solid red');
+    lomakeOK++;
+  }
+  else
+  {
+    el.setAttribute('style', '');
+  }
+};
+
+document.querySelector("form").addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  lomakeOK = 0;
+  checkAttribute('required', document.querySelectorAll('input'), checkEmpty);
+  if (lomakeOK === 0)
+  {
+    document.querySelector("form").submit();
   }
 });
